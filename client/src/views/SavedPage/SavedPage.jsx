a9b8a5c9ee1fc6a06aee2aa0d76e7c80dbd75ba3
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-20 21:55:32 -0700

    redefined views with routing

--- client/src/views/SavedPage/SavedPage.jsx
+++ client/src/views/SavedPage/SavedPage.jsx
@@ -1,16 +1,81 @@
 import React, { Component } from 'react'
 
+//material-ui
+import { Card, Button } from 'react-materialize';
+
+// api
+import API from '../../utils/API.js';
+
 export default class SavedPage extends Component {
+    state = {
+        books: []
+    };
+
+    componentDidMount() {
+        this.loadBooks();
+    }
+
+    loadBooks = () => {
+        API.getSavedBooks()
+            .then(res => {
+                this.setState({ books: res.data });
+                console.log('LOADED BOOKS')
+                console.log(this.state.books);
+            })
+            .catch(err => console.log(err));
+    };
+
+    deleteBookByID = (e) => {
+        e.preventDefault();
+        console.log(e.target.dataset.id);
+    }
 
     render() {
         return (
-            <div style={{ backgroundColor: 'lightgrey' }}> route2
-                <p>saved section</p>
-                <p>map and render saved books from mongo</p>
-                <p>view book</p>
-                <p>delete book</p>
-                <p>title, subinfo, author, image, abstract</p>
-            </div>
+            <>
+                {
+                    this.state.books && this.state.books.length > 0 ?
+
+                        <div className="row mt-5">
+                            {/* map through array of objects and create card */}
+                            {
+                                this.state.books.map(book => (
+                                    <form>
+                                        <Card className="container"
+                                            key={book._id}
+                                            actions={
+                                                [
+                                                    <Button key={book._id}><a href={book.infolink} target="_blank" rel="noopener noreferrer">View</a></Button>,
+                                                    <Button
+                                                        key={book._id}
+                                                        data-id={book._id}
+                                                        onClick={this.deleteBookByID}
+                                                    > Delete</Button >
+
+                                                ]
+                                            }
+                                        >
+                                            <div className="row">
+                                                <div className="col-md-3">
+                                                    <img src={book.imglink} alt="" className="imgclass" />
+                                                </div>
+                                                <div className="col-md-9">
+                                                    <h6>{book.title}</h6>
+                                                    <p>Author: {book.authors}</p>
+                                                    <p>Rating: {book.rating}</p>
+                                                    <p>Desc: {book.description}</p>
+                                                </div>
+                                            </div>
+                                        </Card >
+                                    </form>
+
+                                ))
+                            }
+                        </div>
+
+                        : 'nothing'
+                }
+            </>
         )
     }
 }
--- client/src/views/SearchPage/SearchPage.jsx
+++ client/src/views/SearchPage/SearchPage.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import SearchResults from '../../components/SearchResults/SearchResults.jsx';
-import { TextInput, Button, Icon } from 'react-materialize';
+import { TextInput, Button, Icon, Card } from 'react-materialize';
 
 // api
 import axios from 'axios';
+import API from '../../utils/API.js';
 
 export default class SearchPage extends Component {
-
 
     state = {
         searchInput: '',
@@ -37,6 +36,27 @@ export default class SearchPage extends Component {
             })
     };
 
+    // save book to mongo db
+    saveBook = (e) => {
+        e.preventDefault()
+        const saveBookData = {
+            bookID: e.target.dataset.id,
+            title: e.target.dataset.title,
+            author: e.target.dataset.author,
+            description: e.target.dataset.description,
+            imglink: e.target.dataset.imglink,
+            rating: e.target.dataset.rating,
+            infolink: e.target.dataset.infolink,
+        }
+        console.log(e.target.dataset)
+        API.saveBook(saveBookData)
+            .then(saveBookResponse => {
+                console.log(`API saveBook SUCCESS: ${JSON.stringify(saveBookResponse.data)}`)
+            })
+            .catch(err => { console.log(`API saveBook ERROR: ${err}`) })
+
+    }
+
     render() {
 
         return (
@@ -55,8 +75,56 @@ export default class SearchPage extends Component {
                     </Button>
                 </form>
 
-                <SearchResults searchResults={this.state.searchResults} />
+                {
+                    this.state.searchResults && this.state.searchResults.length > 0 ?
+
+                        <div className="row mt-5">
+                            {/* map through array of objects and create card */}
+                            {
+                                this.state.searchResults.map(book => (
+                                    <form>
+                                        <Card className="container"
+                                            key={'card_' + book.id}
+                                            actions={
+                                                [
+                                                    <Button key={'view_' + book.id}><a href={book.volumeInfo.infoLink} target="_blank" rel="noopener noreferrer">View</a></Button>,
+                                                    <Button
+                                                        key={'save_' + book.id}
+                                                        data-id={book.id}
+                                                        data-title={book.volumeInfo.title}
+                                                        data-author={book.volumeInfo.authors[0]}
+                                                        data-imglink={book.volumeInfo.imageLinks.smallThumbnail ? book.volumeInfo.imageLinks.smallThumbnail : ''}
+                                                        data-rating={book.volumeInfo.averageRating ? book.volumeInfo.averageRating : ''}
+                                                        data-description={book.volumeInfo.description}
+                                                        data-infolink={book.volumeInfo.infoLink}
+                                                        onClick={this.saveBook}
+                                                    > Save</Button >
+
+                                                ]
+                                            }
+                                        >
+                                            <div className="row">
+                                                <div className="col-md-3">
+                                                    <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="" className="imgclass" />
+                                                </div>
+                                                <div className="col-md-9">
+                                                    <h6>{book.volumeInfo.title}</h6>
+                                                    <p>Author: {book.volumeInfo.authors[0]}</p>
+                                                    <p>Rating: {book.volumeInfo.averageRating ? book.volumeInfo.averageRating : 'No Rating'}</p>
+                                                    <p>Desc: {book.volumeInfo.description}</p>
+                                                </div>
+                                            </div>
+                                        </Card >
+                                    </form>
+
+                                ))
+                            }
+                        </div>
+
+                        : 'nothing'
+                }
             </div>
+
         )
     }
 }
