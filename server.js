a9c53ec45c60da9582dc637123cf36831701af92
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-20 21:54:30 -0700

    redefined routes and formatted logs

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
-const routes = require("./routes");
-
 const app = express();
+const booksController = require('./controllers/booksController.js');
+const viewRoutes = require('./routes/view.routes.js');
+const apiRoutes = require('./routes/api.routes.js');
+
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// connect to mongodb
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true, useUnifiedTopology: true });
+// connect mongodb heroku
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/googleBooksDB';
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => { console.log(`MongoDB Connected on ${MONGODB_URI}`) })
+  .catch((err) => { console.log(`MONGODB ERROR: ${err}`) });
 
 // serve static; heroku
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 // routes
-app.use(routes);
+app.use(booksController);
+app.use(apiRoutes);
+app.use(viewRoutes);
 
-// serve index on server start
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}!`);
+  console.log(`
+  -----------------------------
+  Express Server running on: http://localhost:${PORT}!
+  -----------------------------
+  `);
 });
