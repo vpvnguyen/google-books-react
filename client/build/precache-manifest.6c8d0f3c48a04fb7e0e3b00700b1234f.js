c430f1e12b7d13f5639c9870491e4466aba06adf
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-20 21:56:01 -0700

    removed and revised components; migrated logic to each page

---
+++ client/build/precache-manifest.6c8d0f3c48a04fb7e0e3b00700b1234f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a15b077f5e5e9f2071e",
+    "url": "/static/css/main.239a1f3d.chunk.css"
+  },
+  {
+    "revision": "8a15b077f5e5e9f2071e",
+    "url": "/static/js/main.8e39128a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c87b7799a86a7a4ceccc",
+    "url": "/static/css/2.0eb65e34.chunk.css"
+  },
+  {
+    "revision": "c87b7799a86a7a4ceccc",
+    "url": "/static/js/2.cac46ca4.chunk.js"
+  },
+  {
+    "revision": "315947d7e267d779f7d6ca1262fe9f2c",
+    "url": "/index.html"
+  }
+];
