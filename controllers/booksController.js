2d76edf0df801c503f97a09c2a1853433cdc5fa0
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-20 21:52:47 -0700

    CRUD operations with Books MongoDB Model

---
+++ controllers/booksController.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+// book model
+const Book = require('../models/Book.js');
+
+// display all friends list json
+router.get('/getBooks', (req, res) => {
+    Book.find({})
+        .then(books => {
+            console.log('finding books');
+            console.log(books);
+            res.send(books);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+});
+
+router.post('/saveBook/', (req, res) => {
+    console.log(req.body)
+    Book.create(req.body)
+        .then(savedBookReponse => {
+            console.log(`POST /saveBook/ SUCCESS: ${savedBookReponse}`);
+            res.send(savedBookReponse)
+        })
+        .catch(err => {
+            console.log(`POST /saveBook/ ERROR: ${err}`);
+            res.sendStatus(500)
+        });
+
+});
+
+router.get('/getSavedBooks/', (req, res) => {
+
+    Book.find({})
+        .then(books => {
+            console.log('finding books');
+            console.log(books);
+            res.send(books);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+})
+
+module.exports = router;
