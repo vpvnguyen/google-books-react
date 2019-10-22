6f90131aeb17b9aa8f8ff8e4fe24fd8d937826e2
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-21 23:53:21 -0700

    restructured components

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,31 @@
 import React, { Component } from "react";
-import "./App.css";
-// bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+// style
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+// react router
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // components
-import NavBar from './components/NavBar/NavBar.jsx';
 import Header from './components/Header/Header.jsx';
 import SearchPage from './views/SearchPage/SearchPage.jsx';
 import SavedPage from './views/SavedPage/SavedPage.jsx';
 
 class App extends Component {
+
   render() {
     return (
+
       <>
-        <NavBar />
         <Header />
         <Router>
-          <div>
-            <Switch>
-              <Route exact path="/" component={SearchPage} />
-              {/* <Route exact path="/results" component={Results} /> */}
-              <Route exact path="/saved" component={SavedPage} />
-              {/* <Route component={NoMatch} /> */}
-            </Switch>
-          </div>
+          <Switch>
+            <Route exact path="/" component={SearchPage} />
+            <Route exact path="/saved" component={SavedPage} />
+          </Switch>
         </Router>
+
       </>
     );
   }
