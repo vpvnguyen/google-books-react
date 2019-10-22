54f1863cb3b368120d58ffa635c9b996f8c2d990
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-21 23:57:17 -0700

    revised comments

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // connect mongodb heroku
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/googleBooksDB';
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/googleBooksDB`;
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => { console.log(`MongoDB Connected on ${MONGODB_URI}`) })
@@ -24,10 +24,6 @@ if (process.env.NODE_ENV === 'production') {
 // routes
 app.use(booksController);
 app.use(viewRoutes);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 const PORT = process.env.PORT || 3001;
 
