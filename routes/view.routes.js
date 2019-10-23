ee119d5527aa856144a161fd97da708fd5bf5467
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-22 18:07:03 -0700

    trying to fix heroku deployment

--- routes/view.routes.js
+++ routes/view.routes.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 // catch all
 router.get('*', (req, res) => {
+    console.log('catch all' + __dirname)
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
--- server.js
+++ server.js
@@ -17,13 +17,22 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
   .catch(err => console.log(`MONGODB ERROR: ${err}`));
 
 // serve static; heroku
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+// }
 
 // routes
 app.use(booksController);
 app.use(viewRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 
